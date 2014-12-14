325c65ff229394a2ab4221aa562f983803b01fba
motoy3d <motoy3d@gmail.com>
2014-12-15 04:24:20 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'vissel'
+    ,hashtag: 'vissel'
+    ,teamName: '神戸'
+    ,appName: 'スマートJ for ヴィッセル神戸'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id924446607?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.vissel'
+    ,color: '#a62934'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.vissel'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=vissel&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=vissel"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 241842
+    ,nendApiKeyIPhoneIcon: '879425b835da86ede43ee932210c0417517ccd7c'
+    ,nendSpotIdIPhoneBanner: 241841
+    ,nendApiKeyIPhoneBanner: '0864b2441d56b2495dacedccb52d42b4855d826e'
+    //nend(Android)
+    ,nendSpotIdAndroid: 241840
+    ,nendApiKeyAndroid: '7f74fc2a9d137f2b9fa1b053b9210f52b8824761'
+};
