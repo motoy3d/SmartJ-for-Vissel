bfaeec9ee1f304648b25d28a2b884bcc1438c1b8
motoy3d <motoy3d@gmail.com>
2017-03-06 22:43:27 +0900

    動作が遅い部分を改善しました！ 他チームのニュースが見れるようになりました！ 2017シーズンに対応しました！

--- Resources/config.js
+++ Resources/config.js
@@ -2,18 +2,40 @@ exports.config = {
     teamId: 'vissel'
     ,hashtag: 'vissel'
     ,teamName: '神戸'
+    ,teamNameFull: 'ヴィッセル神戸'
     ,appName: 'スマートJ for ヴィッセル神戸'
     ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id924446607?l=ja&ls=1&mt=8'
     ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.vissel'
+    ,backgroundColor: 'black'
+	,visitedBgColor : '#457'
+    ,mainTextColor: 'white'
     ,color: '#a62934'
     ,tintColor: 'white'
+    ,standingsBgcolor: '#a62934'
+    ,standingsTeamFontColor: 'white'
+    ,resultsDetailBtnBgColorActive: '#333'
+    ,resultsDetailBtnColorActive: 'white'
+    ,resultsDetailBtnBgColorInactive: '#202020'
+    ,resultsDetailBtnColorInactive: '#444'
     ,dbName: 'smartj.vissel'
+    //URLベース
+    ,urlBase: 'https://smartj.mobi/redsmylife/'
     //ニュースのソースURL
-    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    ,feedUrlBase: 'https://smartj.mobi/redsmylife/news.json'
     //日程・結果のソースURL
-    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=vissel&season="
+    ,resultsUrl: "https://smartj.mobi/redsmylife/results.json?teamId=vissel&season="
+    //順位表のソースURL
+    ,standingsUrl: 'https://smartj.mobi/redsmylife/standings.json'
     //設定、メッセージのソースURL
-    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=vissel"
+    ,messageUrl: "https://smartj.mobi/redsmylife/config.json?teamId=vissel"
+    //動画のソースURL
+    ,videoUrl: "https://smartj.mobi/redsmylife/video.json?teamId=vissel"
+    //報告URL
+    ,reportUrl: "https://smartj.mobi/redsmylife/report.json?teamId=vissel"
+    //利用規約URL
+    ,rulesUrl: "https://smartj.mobi/redsmylife/rules.jsp?appName="
+    //開発元メールアドレス
+    ,developerMail: "motoy3d+smartj@gmail.com"
     ,newsEntriesPerPage: 50
     ,googleAnalyticsTrackingId: 'UA-30928840-1'
     //nend(iPhone)
@@ -24,4 +46,6 @@ exports.config = {
     //nend(Android)
     ,nendSpotIdAndroid: 241840
     ,nendApiKeyAndroid: '7f74fc2a9d137f2b9fa1b053b9210f52b8824761'
+    ,nendSpotIdAndroidBanner: 393949
+    ,nendApiKeyAndroidBanner: 'c1fee0dc8c030b8742a127777a11ed5933be51de'
 };
